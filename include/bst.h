8c2bd411057d593516f4c2b0669275971c6b5a87
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2020-02-17 15:41:02 -0500

    See todo.rst

--- include/bst.h
+++ include/bst.h
@@ -881,6 +881,7 @@ The node to remove is pnode, found by find(root, key).
 Q: Does the Transplant method apply to C++ (where there is no garabag collection, and we have to manually delete the memory)?
 A: We need to fundamentally understand the algorithm annd not blindly  "believe" it does and translate it to C++. The main remove code
 below, for example, does call transplant.
+Q: Is the algorithm the same technique as the 2nd edition, only slightly reworked? Is it the same as the Corrano algorithm?
 
 */
 template<class Key, class Value> bool bstree<Key, Value>::remove(Key key) noexcept
